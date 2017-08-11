d569508dca01a1895e1d7a1571ffe67394fe3ef4
Giovanni Zunino <giovanni.zunino@chefkoch.de>
2017-08-11 18:16:14 +0200

    Configure build and deployment pipeline

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+/**
+ * Gulpfile for Chefkoch-Design-System
+ *
+ * Compiling of SCSS-Sources
+ */
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+require('es6-promise').polyfill();
+var autoprefixer = require('gulp-autoprefixer');
+var gulpif = require('gulp-if');
+
+// Use 'development' variable with 'gulpif' to toggle writing of sourcemaps
+var development = false;
+
+// Load config
+var config  = {
+    "docsSource": "./components/app/documentation/**/*",
+    "docsDestFolder": "./components/app/web",
+    "CdsSourceFile" : "./components/patterns/chefkoch-design-system.scss",
+    "watchFiles" : ["./components/patterns/**/*", "./components/app/documentation/**/*"],
+    "destFolder" : "./components/app/web/build/",
+    "autoprefixerOptions" : {
+        "browsers" : ["last 3 versions", "> 1% in DE", "Android 4"]
+    }
+};
+
+
+gulp.task('copy-documents', function() {
+    gulp.src(config.docsSource)
+        .pipe(gulp.dest(config.docsDestFolder));
+});
+
+/**
+ * Task: Build
+ * Description: Compiles SCSS to CSS.
+ * Sourcemaps are written if 'development == true'
+ */
+gulp.task('compile-scss', function () {
+    return gulp.src(config.CdsSourceFile)
+        .pipe(gulpif(development, sourcemaps.init()))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sassErrorHandler))
+        .pipe(autoprefixer(config.autoprefixerOptions))
+        .pipe(gulpif(development, sourcemaps.write('./')))
+        .pipe(gulp.dest(config.destFolder));
+});
+
+/**
+ * Custom Error-handler for SASS-Compiler
+ * @param err
+ * @src https://github.com/gulpjs/gulp/issues/259
+ */
+function sassErrorHandler(err) {
+    // Display errormessage
+    console.log(err.message);
+
+    if (development) {
+        // Returns to watcher, when in development-mode
+        this.emit('end');
+    } else {
+        // Stops process when in production-mode
+        process.exit(1);
+    }
+}
+
+/**
+ * Task: Watch
+ * Description: Watcher, sets 'development' so that sourcemaps are written
+ */
+gulp.task('watch', function () {
+    development = true;
+    gulp.watch(config.watchFiles, ['copy-documents', 'compile-scss']);
+});
+
+/**
+ * Task: Build
+ */
+gulp.task('build', ['copy-documents', 'compile-scss']);
